ba64d4242b59ecc59f7f0c68f1fd102b1d9bb940
Mihaela Cocieru <70962062+mihaelac01@users.noreply.github.com>
2020-12-02 20:59:56 +0300

    Add files via upload

---
+++ problema1a.py
@@ -0,0 +1,6 @@
+a=str(input("introdu cuvantul: "))
+b=str(input("introdu litera: "))
+for i in a:
+    c=a.replace(i,b)
+    print(c)
+    
---
+++ problema2a.py
@@ -0,0 +1,8 @@
+import string
+a=str(input("dati un nume:"))
+b=a.title()
+if a==b:
+    print("poate fi un nume")
+else:
+    print("nu poate fi un nume")
+    
